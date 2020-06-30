50ae56d30cbe75c05ccf6ab445d7cafaa83ae279
Luciano Bueno <glamero@gmail.com>
2020-06-30 19:53:46 -0300

    bump

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-theme-dark'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@vue-element-admin/styles/index.scss' // global css
